e68effb5096d2eea6c20fa6ca4bf48aa7faa04c3
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2026-04-15 15:53:00 +0800

    chore: add register rows

--- src/main/kotlin/io/github/asmflow/assembly/armv7/toolWindows/ARMv7RegisterViewToolWindow.kt
+++ src/main/kotlin/io/github/asmflow/assembly/armv7/toolWindows/ARMv7RegisterViewToolWindow.kt
@@ -6,6 +6,52 @@ import javax.swing.JPanel
 object ARMv7RegisterViewToolWindow {
     fun makeContent(): JPanel = panel {
         row {
+            label("R0")
+        }
+        row {
+            label("R1")
+        }
+        row {
+            label("R2")
+        }
+        row {
+            label("R3")
+        }
+        row {
+            label("R4")
+        }
+        row {
+            label("R5")
+        }
+        row {
+            label("R6")
+        }
+        row {
+            label("R7")
+        }
+        row {
+            label("R8")
+        }
+        row {
+            label("R9")
+        }
+        row {
+            label("R10")
+        }
+        row {
+            label("R12")
+        }
+        row {
+            label("FP")
+        }
+        row {
+            label("SP")
+        }
+        row {
+            label("LR")
+        }
+        row {
+            label("PC")
         }
     }
 }
