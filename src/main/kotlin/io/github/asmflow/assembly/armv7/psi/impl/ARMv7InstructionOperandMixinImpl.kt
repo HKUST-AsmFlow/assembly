ed497f8de51e022a0502fd4b69cfcf11e3e32bdb
151044 <yhchowai@connect.ust.hk>
2026-07-09 16:11:22 +0800

    feat: Post-/Pre-indexing & Offset Impl from AST, first pass

--- src/main/kotlin/io/github/asmflow/assembly/armv7/psi/impl/ARMv7InstructionOperandMixinImpl.kt
+++ src/main/kotlin/io/github/asmflow/assembly/armv7/psi/impl/ARMv7InstructionOperandMixinImpl.kt
@@ -3,6 +3,7 @@ package io.github.asmflow.assembly.armv7.psi.impl
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
 import io.github.asmflow.assembly.armv7.execution.ARMv7InstructionOperand
+import io.github.asmflow.assembly.armv7.execution.ARMv7InstructionOperand.Offset
 import io.github.asmflow.assembly.armv7.execution.ARMv7Register
 import io.github.asmflow.assembly.armv7.execution.ARMv7ShiftType
 import io.github.asmflow.assembly.armv7.psi.ARMv7Operand
@@ -11,16 +12,46 @@ import io.github.asmflow.assembly.util.functional.toOption
 import io.github.asmflow.assembly.util.unreachable
 
 abstract class ARMv7InstructionOperandMixinImpl(node: ASTNode) : ASTWrapperPsiElement(node), ARMv7Operand {
-    fun stripRadixPrefix(raw: String): String {
-        val s = raw.trim()
-        return when {
-            s.startsWith("0x", ignoreCase = true) -> s.drop(2)
-            s.startsWith("0b", ignoreCase = true) -> s.drop(2)
-            s.startsWith("0o", ignoreCase = true) -> s.drop(2)
-            s.startsWith("x", ignoreCase = true) -> s.drop(1)
-            s.startsWith("b", ignoreCase = true) -> s.drop(1)
-            s.startsWith("o", ignoreCase = true) -> s.drop(1)
-            else -> s
+    companion object {
+        fun stripRadixPrefix(raw: String): String {
+            val s = raw.trim()
+            return when {
+                s.startsWith("0x", ignoreCase = true) -> s.drop(2)
+                s.startsWith("0b", ignoreCase = true) -> s.drop(2)
+                s.startsWith("0o", ignoreCase = true) -> s.drop(2)
+                s.startsWith("x", ignoreCase = true) -> s.drop(1)
+                s.startsWith("b", ignoreCase = true) -> s.drop(1)
+                s.startsWith("o", ignoreCase = true) -> s.drop(1)
+                else -> s
+            }
+        }
+        fun parseNumericalNode(numberNode: ASTNode): Int {
+            val numberPsi = numberNode.findChildByType(ARMv7TokenTypes.BINARY_NUMBER) ?: numberNode.findChildByType(
+                ARMv7TokenTypes.DECIMAL_NUMBER
+            ) ?: numberNode.findChildByType(ARMv7TokenTypes.HEXADECIMAL_NUMBER) ?: numberNode.findChildByType(
+                ARMv7TokenTypes.OCTAL_NUMBER
+            )!!
+            val isNegative = numberNode.findChildByType(ARMv7TokenTypes.MINUS) != null
+
+            val radix = when (numberPsi.elementType) {
+                ARMv7TokenTypes.BINARY_NUMBER -> 2
+                ARMv7TokenTypes.DECIMAL_NUMBER -> 10
+                ARMv7TokenTypes.HEXADECIMAL_NUMBER -> 16
+                ARMv7TokenTypes.OCTAL_NUMBER -> 8
+                else -> unreachable()
+            }
+
+            return stripRadixPrefix(numberPsi.text).toInt(radix) * (if (isNegative) -1 else 1)
+        }
+
+        fun toOffsetArgs(offsetNode: ASTNode): Pair<ARMv7Register, Offset.Numerical> {
+            val registerPsi = offsetNode.findChildByType(ARMv7TokenTypes.REGISTER)!!.psi
+            val register =
+                ARMv7Register.entries.find { registerPsi.textMatches(it.name.lowercase()) }!!
+
+            val numNode = offsetNode.findChildByType(ARMv7TokenTypes.NUMBER)
+            val num = numNode?.let { parseNumericalNode(it) } ?: 0
+            return Pair(register, Offset.Numerical(num))
         }
     }
 
@@ -32,48 +63,42 @@ abstract class ARMv7InstructionOperandMixinImpl(node: ASTNode) : ASTWrapperPsiEl
 
             number != null -> {
                 val numberNode = number!!.node
-                val numberPsi = numberNode.findChildByType(ARMv7TokenTypes.BINARY_NUMBER) ?: numberNode.findChildByType(
-                    ARMv7TokenTypes.DECIMAL_NUMBER
-                ) ?: numberNode.findChildByType(ARMv7TokenTypes.HEXADECIMAL_NUMBER) ?: numberNode.findChildByType(
-                    ARMv7TokenTypes.OCTAL_NUMBER
-                )!!
+                val num = parseNumericalNode(numberNode)
 
-                val radix = when (numberPsi.elementType) {
-                    ARMv7TokenTypes.BINARY_NUMBER -> 2
-                    ARMv7TokenTypes.DECIMAL_NUMBER -> 10
-                    ARMv7TokenTypes.HEXADECIMAL_NUMBER -> 16
-                    ARMv7TokenTypes.OCTAL_NUMBER -> 8
-                    else -> unreachable()
-                }
-
-                ARMv7InstructionOperand.Number(value = stripRadixPrefix(numberPsi.text).toInt(radix))
+                ARMv7InstructionOperand.Number(num)
             }
 
             offset != null -> {
-                TODO("")
+                val (reg, num) = toOffsetArgs(offset!!.node)
+                ARMv7InstructionOperand.RegisterWithOffset(reg, num,
+                    ARMv7InstructionOperand.AddressingFlags(preIndexed = false, postIndexed = false))
             }
 
             offsetVariant != null -> {
-                TODO("")
+                // Deal with this later
+                TODO("Offset variants are not yet supported!")
             }
 
             postindexed != null -> {
-                TODO("")
+                val (reg, num) = toOffsetArgs(postindexed!!.node)
+                ARMv7InstructionOperand.RegisterWithOffset(reg, num,
+                    ARMv7InstructionOperand.AddressingFlags(preIndexed = false, postIndexed = true))
             }
 
             preindexed != null -> {
-                TODO("")
+                val (reg, num) = toOffsetArgs(preindexed!!.node)
+                ARMv7InstructionOperand.RegisterWithOffset(reg, num,
+                    ARMv7InstructionOperand.AddressingFlags(preIndexed = true, postIndexed = false))
             }
 
             registerWithShift != null -> {
                 val registerPsi =
-                    registerWithShift!!.node.findChildByType(ARMv7TokenTypes.REGISTER).toOption().unwrap().psi
+                    registerWithShift!!.node.findChildByType(ARMv7TokenTypes.REGISTER)!!.psi
                 val register =
-                    ARMv7Register.entries.find { registerPsi.textMatches(it.name.lowercase()) }.toOption().unwrap()
+                    ARMv7Register.entries.find { registerPsi.textMatches(it.name.lowercase()) }!!
                 val pair = registerWithShift!!.shift.toOption().map { psi ->
                     val shiftType =
-                        ARMv7ShiftType.entries.find { psi.shiftType.textMatches(it.name.lowercase()) }.toOption()
-                            .unwrap()
+                        ARMv7ShiftType.entries.find { psi.shiftType.textMatches(it.name.lowercase()) }!!
                     // todo: register from psi to armv7register enum
                     Pair(shiftType, TODO())
                 }
