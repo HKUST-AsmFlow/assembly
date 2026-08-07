f640a805cd44384797fce070b58103f61c553f04
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2026-08-06 23:55:44 -0700

    chore: add memory view stub

---
+++ src/main/kotlin/io/github/asmflow/assembly/armv7/toolWindows/ARMv7MemoryViewToolWindow.kt
@@ -0,0 +1,4 @@
+package io.github.asmflow.assembly.armv7.toolWindows
+
+class ARMv7MemoryViewToolWindow {
+}
---
+++ src/main/kotlin/io/github/asmflow/assembly/armv7/toolWindows/ARMv7MemoryViewToolWindowFactory.kt
@@ -0,0 +1,11 @@
+package io.github.asmflow.assembly.armv7.toolWindows
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import io.github.asmflow.assembly.openapi.wm.AssemblyToolWindowFactory
+
+class ARMv7MemoryViewToolWindowFactory : AssemblyToolWindowFactory {
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        TODO("Not yet implemented")
+    }
+}
