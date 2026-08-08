46d0dee50c516b251816f1cf5c8d0de16e9da8aa
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2026-08-07 23:42:08 -0700

    feat: try add memory view

--- src/main/kotlin/io/github/asmflow/assembly/armv7/toolWindows/ARMv7MemoryViewTableModel.kt
+++ src/main/kotlin/io/github/asmflow/assembly/armv7/toolWindows/ARMv7MemoryViewTableModel.kt
@@ -6,19 +6,30 @@ import javax.swing.table.AbstractTableModel
 class ARMv7MemoryViewTableModel(
     initialMemory: ARMv7MemoryState
 ) : AbstractTableModel() {
+    val wordsPerRow = 8
+    val rows = 256
+
     private var memory: ARMv7MemoryState = initialMemory
 
+    override fun getColumnCount(): Int = wordsPerRow + 1
 
-    override fun getColumnCount(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun getRowCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getRowCount(): Int = rows
 
     override fun getValueAt(rowIndex: Int, columnIndex: Int): Any {
-        TODO("Not yet implemented")
+        return when (columnIndex) {
+            0 -> {
+                val address = 0u + (rowIndex * wordsPerRow * 4).toUInt()
+                "0x%08X".format(address)
+            }
+
+            in 1..wordsPerRow -> {
+                val rowAddress = 0u + (rowIndex * wordsPerRow * 4).toUInt()
+                val address = rowAddress + ((columnIndex - 1) * 4).toUInt()
+                "0x%08X".format(memory.getWord(address))
+            }
+
+            else -> throw IllegalArgumentException("Unexpected column index: $columnIndex")
+        }
     }
 
     fun updateMemoryData(newMemoryState: ARMv7MemoryState) {
--- src/main/kotlin/io/github/asmflow/assembly/armv7/toolWindows/ARMv7MemoryViewToolWindow.kt
+++ src/main/kotlin/io/github/asmflow/assembly/armv7/toolWindows/ARMv7MemoryViewToolWindow.kt
@@ -1,11 +1,39 @@
 package io.github.asmflow.assembly.armv7.toolWindows
 
+import com.intellij.ui.components.JBScrollPane
+import com.intellij.ui.dsl.builder.Align
 import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.table.JBTable
+import com.intellij.util.ui.JBUI
 import io.github.asmflow.assembly.armv7.emulator.ARMv7MemoryState
 import javax.swing.JPanel
+import javax.swing.JTable
 
 class ARMv7MemoryViewToolWindow {
-    fun getContent(): JPanel = panel {  }
+    // TODO: text.
+    private val tableModel =
+        ARMv7MemoryViewTableModel(ARMv7MemoryState(emptyList()))
+    private val table = JBTable(tableModel)
+    private val contentPanel: JPanel
 
-    fun updateState(state: ARMv7MemoryState) {}
+    init {
+        table.autoResizeMode = JTable.AUTO_RESIZE_LAST_COLUMN
+        val scrollPane = JBScrollPane(table)
+
+        contentPanel = panel {
+            row {
+                cell(scrollPane).align(Align.FILL)
+            }.resizableRow()
+        }
+
+        contentPanel.border = JBUI.Borders.empty(JBUI.insets(5, 10))
+    }
+
+    fun getContent(): JPanel {
+        return contentPanel
+    }
+
+    fun updateState(state: ARMv7MemoryState) {
+        tableModel.updateMemoryData(state)
+    }
 }
