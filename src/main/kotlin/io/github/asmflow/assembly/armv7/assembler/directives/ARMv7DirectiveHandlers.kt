541574d819107d196e00c6df38bbc662c5280d0c
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2026-09-27 02:08:31 +0800

    chore: add stub for data directive handler example

--- src/main/kotlin/io/github/asmflow/assembly/armv7/assembler/directives/ARMv7DirectiveHandlers.kt
+++ src/main/kotlin/io/github/asmflow/assembly/armv7/assembler/directives/ARMv7DirectiveHandlers.kt
@@ -4,8 +4,12 @@ import io.github.asmflow.assembly.armv7.util.functional.toOption
 
 object ARMv7DirectiveHandlers {
     private val handlers = mapOf(
+        // size 0
         "data" to ARMv7DataSectionSwitchHandler,
-        "text" to ARMv7TextSectionSwitchHandler
+        "text" to ARMv7TextSectionSwitchHandler,
+
+        // size 1
+        "byte" to ARMv7ByteDataDirectiveHandler,
     )
 
     fun get(name: String) = handlers[name].toOption()
